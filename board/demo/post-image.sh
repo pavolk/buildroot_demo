73474743c33b410b736fa0e28b3c2b17833f79d9
Pavol Kurina <pavol.kurina@gmail.com>
2019-05-20 19:03:28 +0200

    fixed post-image.sh

--- board/demo/post-image.sh
+++ board/demo/post-image.sh
@@ -9,6 +9,7 @@ MKBOOTIMAGE=${BUILD_DIR}/../../utils/mkbootimage
 cp ${BOARD_DIR}/pre-built/linux/implementation/download.bit ${BINARIES_DIR}/fpga.bit
 cp ${BOARD_DIR}/pre-built/linux/images/zynq_fsbl.elf ${BINARIES_DIR}/fsbl.elf
 cp ${BOARD_DIR}/pre-built/linux/images/u-boot.elf ${BINARIES_DIR}/u-boot.elf
+cp ${BOARD_DIR}/pre-built/linux/images/system.dtb ${BINARIES_DIR}/devicetree.dtb 
 
 echo "Creating boot.bin..."
 cp ${BOARD_DIR}/boot.bif ${BINARIES_DIR}
@@ -17,8 +18,6 @@ cp ${BOARD_DIR}/boot.bif ${BINARIES_DIR}
 echo "Creating FIT-image..."
 cp ${BOARD_DIR}/image.its ${BINARIES_DIR}
 (cd ${BINARIES_DIR}; ${MKIMAGE} -f image.its image.ub)
-
-cp ${BOARD_DIR}/pre-built/linux/images/system.dtb ${BINARIES_DIR}/devicetree.dtb 
 
 echo "Creating the sd-card image..."
 support/scripts/genimage.sh -c ${BOARD_DIR}/genimage.cfg
